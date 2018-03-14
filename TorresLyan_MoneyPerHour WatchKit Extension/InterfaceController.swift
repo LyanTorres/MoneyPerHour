fb7b72227742e63882dd83407a5b4e8e32107d25
Lyan Torres Labiosa <ltorreslabiosa@fullsail.edu>
2018-03-13 23:07:52 -0400

    trying to push up rest of project

---
+++ TorresLyan_MoneyPerHour WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TorresLyan_MoneyPerHour WatchKit Extension
+//
+//  Created by Lyan Torres on 3/12/18.
+//  Copyright © 2018 Lyan Torres. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
